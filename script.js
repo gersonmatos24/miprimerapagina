59e7b2500a5d33a31c32c5f6f481f0175ea1d051
gersonmatos24 <matosgerson661@gmail.com>
2024-03-12 19:50:45 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//alert("Hola este es mi Javascript");
+//let nombre = "Gerson";
+//nombre = "Matos"
+//var nombre1 = "Gerson";
+//const nombre2 = "Matos";
+
+//console.log(nombre)
+//console.log(nombre1)
+//console.log(nombre2)
+
+//SELECCIONAR ELEMENTOS 
+
+let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector("logo.fuente-")
+titulo.innerHTML = contenidoTitulo;
+
+
+//CONDICIONALES
+
+let textoTitulo = titulo.innerText;
+console.log("textoTitulo")
